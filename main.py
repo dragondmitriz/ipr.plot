89009eebb88fcc6f81bb24a82e3b491895a94473
dmitriz <dmitriz.malyshko@mail.ru>
2019-12-27 07:33:30 +0300

    clear main

--- main.py
+++ main.py
@@ -1,21 +1,4 @@
 from lib.metric_parser import *
-import matplotlib.pyplot as plt
-
-# index_graph = [0, -1]
-#
-#
-# def update_index_graph(index_gr):
-#     if index_gr[1] >= size[1] - 1:
-#         index_gr[1] = 0
-#         index_gr[0] += 1
-#     else:
-#         index_gr[1] += 1
-#     if index_gr[0] >= size[0]:
-#         plt.show()
-#         fig = plt.figure(figsize=(16, 9))
-#         index_gr[0] = 0
-#         index_gr[1] = 0
-#     return (index_gr[0], index_gr[1])
 
 
 sar = SarMetrics()
